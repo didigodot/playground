70085c3900028b5fcd2494cd590521ac5d115b86
Dmitri Park <cellofortist@gmail.com>
2013-05-28 15:19:19 -0400

    made it shorter...

--- KnightTour.java
+++ KnightTour.java
@@ -23,27 +23,12 @@ public class KnightTour
         System.out.println("---------------");
    
     }
-	 public static boolean checkFill(int[][] a)
-	 {
-	 	boolean b = true; //assume it's filled until proved otherwise	
-		   for(int x = 0; x<8; x++)
-			for(int y = 0; y<8; y++)
-				{
-					if(a[x][y]==0)
-					{
-						b = false;
-						return b;
-					}
-				}
-		return b;
-	 }
-    	
-    public static void move(int[][] board, int[] pos, int movenumber)
+	public static void move(int[][] board, int[] pos, int movenumber)
     {
         int[] oldpos = pos;
         board[pos[0]][pos[1]] = movenumber;
         drawBoard(board);
-        if(checkFill(board)==true)
+        if(movenumber==64)
         {
             drawBoard(board);
         }
@@ -52,9 +37,8 @@ public class KnightTour
             for(int i = 0; i<8; i++)
             {
                 int newx = pos[0]+moves[0][i];
-                System.out.println(moves[0][i]+", "+moves[1][i]);
                 int newy = pos[1]+moves[1][i];
-                // if bounded and not visisted before
+                // if bounded and not visited before
                 if(newx>=0 && newx<8 &&
                    newy>=0 && newy<8 &&
                    board[newx][newy]==0)
