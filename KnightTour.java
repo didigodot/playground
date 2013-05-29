0ec3865951f5f94df93d19e556623107abd88b47
Dmitri Park <cellofortist@gmail.com>
2013-05-29 12:39:52 -0400

    five

--- KnightTour.java
+++ KnightTour.java
@@ -4,11 +4,14 @@ import java.lang.*;
 public class KnightTour
 {
     static int[][] moves = new int[][]{{1, 1, -1, -1, 2, 2, -2, -2},{2, -2, 2, -2, 1, -1, 1, -1}}; 
+    static int size = 5;
+    static int[][] board = new int[size][size];
+
     public static void drawBoard(int[][] board)
     {
-     for(int y = 0; y<8; y++)
+     for(int y = 0; y<size; y++)
             {   
-            for(int x = 0; x<8; x++)
+            for(int x = 0; x<size; x++)
                 {
                     if(x==0 && y>0)
                         System.out.println("");
@@ -23,14 +26,27 @@ public class KnightTour
         System.out.println("---------------");
    
     }
-	public static void move(int[][] board, int[] pos, int movenumber)
+    public static void shuffle()
     {
-        int[] oldpos = pos;
+        for(int i = 0; i<15; i++)
+        {
+            int a = (int)(Math.random()*size);
+            int b = (int)(Math.random()*size);
+            int[] temp = {moves[0][a], moves[1][a]};
+            moves[0][a]=moves[0][b];
+            moves[1][a]=moves[1][b];
+            moves[0][b] = temp[0];
+            moves[1][b]=temp[1];
+        }
+    }
+	public static void move(int[] pos, int movenumber)
+    {
         board[pos[0]][pos[1]] = movenumber;
         drawBoard(board);
-        if(movenumber==64)
+        if(movenumber==size*size)
         {
             drawBoard(board);
+            return;
         }
         else
         {
@@ -39,12 +55,13 @@ public class KnightTour
                 int newx = pos[0]+moves[0][i];
                 int newy = pos[1]+moves[1][i];
                 // if bounded and not visited before
-                if(newx>=0 && newx<8 &&
-                   newy>=0 && newy<8 &&
+                if(newx>=0 && newx<size &&
+                   newy>=0 && newy<size &&
                    board[newx][newy]==0)
                 {       
                  int[] x = {newx, newy};
-                 move(board, x, movenumber+1);
+                 move(x, movenumber+1);
+                 //shuffle();
                 }
             }
         }
@@ -53,9 +70,15 @@ public class KnightTour
       
     public static void main(String[] args)
     {
-        int[][] board = new int[8][8];
-        //int[] pos = {(int)(Math.random()*7), (int)(Math.random()*7)};
-        int[] pos = {0,0};
-        move(board, pos, 1);
+        int[] pos = {(int)(Math.random()*size), (int)(Math.random()*size)};
+        for(int i = 0; i<size; i++)
+        {
+            for(int y = 0;y<size; y++)
+            {
+                board[i][y]=0;
+            }
+        }
+        //int[] pos = {0,0};
+        move(pos, 1);
     }
 }
